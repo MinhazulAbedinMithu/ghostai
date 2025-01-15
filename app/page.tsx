72b369e1df69129b626da4df435386eb96219f1c
Minhazul Abedin <minhazulabedinmithu@gmail.com>
2025-01-16 02:01:49 +0600

    nav menu button to apear section

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,14 @@ import { useState } from "react";
 export default function Home() {
   const [terminalMode, setTerminalMode] =  useState<null | "terminal" |"stream">(null);
   return (
-    <div className="flex flex-col gap-5 pt-3 text-emerald-400 text-2xl font-bold">
-      <div className="flex items-center justify-center gap-4 md:gap-14">
+    <div className="flex flex-col gap-5 pt-3 text-2xl font-bold">
+      {terminalMode ? null : <div className="flex items-center justify-center gap-4 md:gap-14">
         
-          <button onClick={() => setTerminalMode("terminal")} className="bg-slate-800/80 px-5 py-3 rounded-xl">About</button>
-          <button onClick={() => setTerminalMode("stream")} className="bg-slate-800/80 px-5 py-3 rounded-xl">Stream</button>
-          
+        <button onClick={() => setTerminalMode("terminal")} className="bg-slate-800/95 px-5 py-3 rounded-xl text-purple-500">About</button>
+        <button onClick={() => setTerminalMode("stream")} className="bg-slate-800/95 px-5 py-3 rounded-xl text-pink-700">Live Stream</button>
+        
 
-      </div>
+    </div>}
       <div className="md:flex items-center justify-center h-full w-full">
     <div className="md:max-w-3xl md:mx-auto w-full">
       {/* <div className="grid grid-cols-1 md:grid-cols-2 items-stretch gap-6 md:gap-10 px-4 md:px-10 py-6">
