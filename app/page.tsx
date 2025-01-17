4b7f2312e0c5b958b18b88ba37c8829ee0255eb1
Minhazul Abedin <minhazulabedinmithu@gmail.com>
2025-01-17 16:30:25 +0600

    added

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,14 @@ import { useState } from "react";
 export default function Home() {
   const [terminalMode, setTerminalMode] =  useState<null | "terminal" |"stream">("stream");
   return (
-    <div className="flex flex-col gap-5 pt-3 text-3xl font-bold relative">
-      {terminalMode ? null : <div className="flex items-center burbanfont justify-center gap-4 md:gap-14">
+    <div className="flex flex-col gap-5 pt-3 text-3xl font-bold relative max-w-7xl w-full mx-auto">
+      <div className="flex items-center burbanfont justify-end gap-4 md:gap-14">
         
-        <button onClick={() => setTerminalMode("terminal")} className="bg-slate-800/95 px-5 py-3 rounded-xl text-fuchsia-300">About</button>
-        <button onClick={() => setTerminalMode("stream")} className="bg-slate-800/95 px-5 py-3 rounded-xl text-fuchsia-300">Live Stream</button>
+        <button onClick={() => terminalMode==="stream" ? setTerminalMode("terminal") : setTerminalMode("stream")} className="bg-slate-800/95 px-5 py-3 rounded-xl text-fuchsia-300">Menu</button>
+        {/* <button onClick={() => setTerminalMode("stream")} className="bg-slate-800/95 px-5 py-3 rounded-xl text-fuchsia-300">Live Stream</button> */}
         
 
-    </div>}
+    </div>
       <div className="md:flex items-end justify-end h-full w-full">
     <div className="md:max-w-3xl md:mx-auto w-full relative">
       {/* <div className="grid grid-cols-1 md:grid-cols-2 items-stretch gap-6 md:gap-10 px-4 md:px-10 py-6">
@@ -21,12 +21,12 @@ export default function Home() {
         <RightContent />
       </div> */}
       <div className="flex items-center flex-col justify-center gap-8">
-        {terminalMode === "terminal" ? <LeftContent/>: terminalMode === "stream" ? <div className="max-w-[550px] w-full h-[400px] md:absolute top-[50px]  xl:-left-[38%] 2xl:-left-[58%] flex items-center justify-center flex-col gap-4">
+        {terminalMode === "terminal" ? <LeftContent/>: terminalMode === "stream" ? <div className="max-w-[500px] 2xl:max-w-[600px] w-full h-[420px] md:absolute top-[-40px] 2xl:top-[50px] lg:left-20  xl:-left-[38%] 2xl:-left-[58%] flex items-center justify-center flex-col gap-4">
           <RightContent/>
-          <button className="bg-black text-white px-3 py-1 burbanfont" onClick={() => setTerminalMode(null)}>Exit</button>
+          {/* <button className="bg-black text-white px-3 py-1 burbanfont" onClick={() => setTerminalMode(null)}>Exit</button> */}
         </div> : null}
 
-        {terminalMode==="terminal" && <button className="bg-black text-white px-3 py-1 burbanfont" onClick={() => setTerminalMode(null)}>Exit</button>}
+        {terminalMode==="terminal" && <button className="bg-black text-white px-3 py-1 burbanfont" onClick={() => setTerminalMode("terminal")}>Exit</button>}
       </div>
       {/**
        * <div className="grid grid-cols-1 md:grid-cols-3 items-stretch gap-6 md:gap-10 px-4 md:px-10 py-6">
